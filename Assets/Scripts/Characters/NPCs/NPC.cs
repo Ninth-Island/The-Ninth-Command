aee6bf2fbfc193974d03130161d26da03df87c27
Ni_09 <89614341+Ninth-Island@users.noreply.github.com>
2022-03-19 19:44:21 -0700

    Fixed Healthbar reverse

--- Assets/Scripts/Characters/NPCs/NPC.cs
+++ Assets/Scripts/Characters/NPCs/NPC.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
 public class NPC : Character{
+
+    [SerializeField] private bool isPatrolling;
+
+    private BoxCollider2D GroundChecker;
     
     private GameObject healthBG;
     private GameObject healthFill;
 
     private Coroutine HealthRoutine;
-    
+
 
 
     protected override void Start(){
         base.Start();
-        
+
+        GroundChecker = transform.GetChild(4).GetComponent<BoxCollider2D>();
+
         healthBG = transform.GetChild(2).gameObject;
         healthFill = transform.GetChild(3).gameObject;
 
@@ -26,18 +32,20 @@ public class NPC : Character{
         UpdateHealthBar();
     }
     
+
     protected override void FixedUpdate(){
         base.FixedUpdate();
+        Patrol();
 
     }
-    
+
     protected override void TakeDamage(int damage){
         base.TakeDamage(damage);
-        
+
         if (HealthRoutine != null){
             StopCoroutine(HealthRoutine);
         }
-        
+
         HealthRoutine = StartCoroutine(ShowHealthBar());
     }
 
@@ -45,20 +53,23 @@ public class NPC : Character{
 
         Vector3 scale = healthFill.transform.localScale;
         Vector3 pos = healthFill.transform.localPosition;
-        
+
         float newScale = (float) health / maxhealth;
         float newPos = 2 - newScale * 2;
-        
-        
+        if (transform.localScale.x > 0){
+            newPos *= -1;
+        }
+
+
         healthFill.transform.localScale = new Vector3(newScale * 4, scale.y, scale.x);
         healthFill.transform.localPosition = new Vector3(newPos, pos.y, pos.z);
     }
 
     private IEnumerator ShowHealthBar(){
-        
+
         healthBG.SetActive(true);
         healthFill.SetActive(true);
-        
+
         UpdateHealthBar();
 
         yield return new WaitForSeconds(3);
@@ -67,7 +78,34 @@ public class NPC : Character{
         healthFill.SetActive(false);
     }
 
+    #region Patrolling
 
+    private void Patrol(){
+        if (isPatrolling){
+            WallCheck();
+            GroundCheck();
+            Body.velocity = new Vector2(moveSpeed * transform.localScale.x, Body.velocity.y);
+        }
+    }
+
+    private void WallCheck(){
+        RaycastHit2D raycastHit = Physics2D.CircleCast(transform.position, 2, Vector2.right * transform.localScale.x, 1,
+            LayerMask.GetMask("Ground", "Vehicle Outer"));
+
+        if (raycastHit.collider){
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+
+    private void GroundCheck(){
+        if (!Physics2D.IsTouchingLayers(GroundChecker, LayerMask.GetMask("Ground")) && !Airborne){
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y);
+
+        }
+    }
+
+
+    #endregion
 
 
     
