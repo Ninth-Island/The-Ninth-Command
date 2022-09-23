3f2b8f7b77d6ba7b5786157ef052f5fcd576b580
Ni_09 <89614341+Ninth-Island@users.noreply.github.com>
2022-09-23 12:05:08 -0700

    big backup before

--- Assets/Scripts/Projectiles/Bullet.cs
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -28,9 +28,11 @@ public class Bullet : Projectile{
         GameObject db = Instantiate(deadBullet, transform.position, transform.rotation);
         db.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity / 3;
         Destroy(db, 2f);
-        StartCoroutine(ServerDestroy(gameObject, 0));
+        if (isServer){
+            StartCoroutine(ServerDestroy(gameObject, 0));
+        }
     }
-    
+
     [Server]
     public override void SetValues(int damage, float speed, float angle, bool piercing, int firedLayer, string name){
         base.SetValues(damage, speed, angle, piercing, firedLayer, name);
